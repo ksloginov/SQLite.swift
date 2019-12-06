2f33668a06b265ff2af3b125d7f73452d570cf39
Konstantin Loginov <loginov.k@gmail.com>
2019-12-06 13:32:02 +0100

    Trying to unblock our unit-tests

--- Package.swift
+++ Package.swift
@@ -11,14 +11,3 @@ let package = Package(
     ],
     swiftLanguageVersions: [4, 5]
 )
-
-#if os(Linux)
-    package.dependencies = [.package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3")]
-    package.targets = [
-        .target(name: "SQLite", exclude: ["Extensions/FTS4.swift", "Extensions/FTS5.swift"]),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests", exclude: [
-            "FTS4Tests.swift",
-            "FTS5Tests.swift"
-        ])
-    ]
-#endif
